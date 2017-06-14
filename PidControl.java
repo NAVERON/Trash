20ce59c29ebaba7c75d8497429cadb32ca2c330e
NAVERON <2046783134@qq.com>
2017-06-14 22:55:55 +0800

    更新PID算法

--- PidControl.java
+++ PidControl.java
@@ -1,8 +1,15 @@
 package show;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -40,7 +47,7 @@ public class PidControl extends JPanel{
 		frame.setContentPane(draw);
 		
 		//frame.pack();
-		frame.setBounds(50, 50, 1000, 600);
+		frame.setBounds(50, 50, 1200, 700);
 		frame.setDefaultCloseOperation(3);
 		frame.setVisible(true);
 		
@@ -53,6 +60,8 @@ public class PidControl extends JPanel{
 		//draw.pidAdd();
 		//draw.repaint();
 		draw.repaint();
+		
+		//draw.toFile(draw.value, draw.thetas);
 	}
 	
 	public void test(){  //测试方法
@@ -207,19 +216,19 @@ public class PidControl extends JPanel{
 			repaint();
 		}
 	}
-	public void pid(){  //稳定快速准确
+	
+	public void pid(){  //稳定快速准确----->K=0.0785F, T = 3.12F;  //船舶操纵性能，根据操纵性能求出舵角theta后->角度变化
 		uArray.clear();
 		value.clear();
 		desireValue=80;
 		curValue=0;
-		
-		float a = kp*(1+1/ki+kd);
+		//K = 0.1F;T = 3F;
+		/*float a = kp*(1+1/ki+kd);
 		float b = kp*(1+2*kd);
-		float c = kp*kd;
-		boolean begin=true;
-		while(Math.abs(curValue-desireValue)>1 || begin){
+		float c = kp*kd;*/
+		while( !(Math.abs(curValue-desireValue)<1 && Math.abs(theta)<0.5) ){
 			cur_error = desireValue-curValue;  //当前误差
-			cur_u = kp*(cur_error-last_error)+(kp/ki)*cur_error+kp*kd*(cur_error-2*last_error+pre_error);  //计算得出增量
+			cur_u = kp*(cur_error-last_error)+(kp/ki)*cur_error+kp*kd*(cur_error-2*last_error+pre_error);   //计算得出增量
 			//cur_u = a*cur_error-b*last_error+c*pre_error;
 			pre_error=last_error;
 			last_error=cur_error;
@@ -228,24 +237,12 @@ public class PidControl extends JPanel{
 			if (theta>35 || theta<-35) {  //当增大到最大舵角后不再增大-->这里有一个问题，并不是舵角越大转角效果越好？怎么办？映射函数？
 				theta -=cur_u;
 			}
-			//根据舵角计算下一次采集的航向值
+			//根据舵角计算下一次采集的航向值---->
 			float div = (float) (K*theta*(2-T+T*Math.pow(Math.E, -2/T)));
-			/*float div=0;
-			if (theta>0 && theta<10) {  //简单计算在舵角一定的情况下，时间t后航向变化值
-				div=5;
-			}
-			else if(theta>10 && theta<20){
-				div=10;
-			}
-			else if(theta>20 && theta<35){
-				div=4;
-			}*/
-			curValue += div;
+			
+			curValue += div;  //计算当前航向角
 			float r = (float) (K*theta*(1-Math.exp(-2/T)));
 			System.out.println(r);
-			if (r<0.01 && Math.abs(curValue-desireValue)<1) {
-				begin=false;
-			}
 			System.out.println("+-------------------------------"
 					+"\n|当前值："+curValue
 					+"\n|本次增量-->"+cur_u
@@ -253,17 +250,18 @@ public class PidControl extends JPanel{
 					+"\n+------------------------------------"
 					);
 			uArray.add(cur_error);  //当前的误差
-			value.add(curValue);
-			thetas.add(theta);
-			try {
+			value.add(curValue);  //当前航向角
+			thetas.add(theta);  //当前舵角
+			/*try {
 				Thread.sleep(200);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			repaint();
+			repaint();*/
 		}
 	}
+	
 	public void pidAdd(){  //位置式
 		uArray.clear();
 		value.clear();
@@ -300,85 +298,145 @@ public class PidControl extends JPanel{
 		}
 	}
 	
-	/*public void curchange(){  //计算舵角大小
-		if (theta>-5 && theta<5) {
-			curValue += 1;
-		}
-		else if (theta>=5 && theta<10) {
-			curValue += 5;
-		}
-		else if (theta>=10 && theta<20) {
-			curValue += 10;
-		}
-		else if (theta>=20 && theta<35) {
-			curValue += 5;
-		}/////////////////////////////////////////////////////////////////////
-		else if (theta<=-5 && theta>-10) {
-			curValue += -5;
-		}
-		else if (theta<=-10 && theta>-20) {
-			curValue  += -10;
-		}
-		else if (theta<=-20 && theta>-35) {
-			curValue += -5;
-		}
-		else {
-			return;
-		}
-	}*/
 	
 	@Override
-	public void paint(Graphics g) {
-		super.paint(g);  //去掉这句不会全部重新绘制，只会绘制新值，并覆盖原值
+	public void paint(Graphics graphics) {
+		super.paint(graphics);  //去掉这句不会全部重新绘制，只会绘制新值，并覆盖原值
 		//**************************************************
 		//坐标系，左下角  0 500处-------->画出坐标系
 		//x轴
-		g.setColor(Color.BLACK);
-		g.drawLine(0, 500, 700, 500);
+		Graphics2D g = (Graphics2D) graphics.create();
 		
-		for(int i=0;i<700;i+=40){
-			g.drawOval(i, 500, 5, 5);
-			g.drawString(String.valueOf(i), i, 520);
+		g.setColor(Color.BLACK);
+		//x轴横线
+		g.drawLine(40, 500, 1200, 500);
+		//x坐标点
+		g.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		for(int i=0;i<1200;i+=40){
+			//g.drawOval(i+18, 500, 5, 5);
+			g.fillOval(i+40, 500, 3, 3);
+			g.drawString(String.valueOf(i*2/40), i+40, 520);
 		}
 		//y轴
-		for(int i=0;i<501;i+=20){
-			g.drawOval(0, 500-i, 5, 5);
-			g.drawString(String.valueOf(i/5), 15, 510-i);
+		for(int i=0;i<501;i+=25){
+			//g.drawOval(20, 500-i, 5, 5);
+			g.fillOval(40, 500-i, 3, 3);
+			g.drawString(String.valueOf(i/5), 10, 510-i);
 		}
-		g.setFont(new Font("Consolas", Font.PLAIN, 20));
-		g.drawString("kp = "+kp
-				+ "->ki = "+ki
-				+ "->kd = "+kd,
-				600, 20);  //当前pid的值
-		g.drawString("cur error", 600, 40);
-		g.drawString("Current Value", 600, 60);
-		g.drawString("Rudder Angle", 600, 80);
+		//g.setStroke(new BasicStroke(3.0F));
+		g.drawLine(40, 500, 40, 0);
+		/************************坐标画完了*********************************/
 		//显示数据--------------------------------------------------------------
-		g.drawLine(0, (int) (500-desireValue*5), 700, (int) (500-desireValue*5));  //目标线
-		g.setColor(Color.BLUE);
-		g.drawLine(750, 40, 850, 40);  //表示那种颜色对应哪种变量
-		int j=0;
-		for(int i=0;i<uArray.size();i++){  //增量值
+		/*g.setFont(new Font("宋体", Font.PLAIN, 18));
+		g.setStroke(new BasicStroke(3.0F));
+		
+		g.drawString("时间 / s", 850, 550);
+		g.drawString("舵角及航向角度数 / °", 40, 40);
+		g.drawString("kp = "+kp
+				+ "     ki = "+ki
+				+ "     kd = "+kd,
+				750, 350);  //当前pid的值
+		//g.drawString("cur error", 600, 40);
+		g.drawString("目标航向", 750, 200);
+		g.drawString("当前航向", 750, 250);
+		g.drawString("舵角", 750, 300);*/
+		g.setStroke(new BasicStroke(2.0F));
+		g.setColor(Color.RED);
+		//g.drawLine(900, 200, 1000, 200);
+		g.drawLine(0, (int) (500-desireValue*5), 1200, (int) (500-desireValue*5));  //目标线
+		//g.setColor(Color.BLUE);
+		//g.drawLine(750, 40, 850, 40);  //表示那种颜色对应哪种变量
+		g.setColor(Color.BLACK);
+		/************************************************************/
+		Stroke backup = g.getStroke();
+		/*Stroke dash = new BasicStroke(3.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_ROUND, 2.5f, new float[] { 5, 20, },
+                0f);
+        g.setStroke(dash);*/
+		float j=0;
+		/*for(int i=0;i<uArray.size();i++){  //增量值
 			float geti = uArray.get(i);  //自动拆箱
 			g.drawOval(j, 500-(int) geti*5, 5, 5);
 			j+=5;
-		}
+		}*/
 		g.setColor(Color.GREEN);
-		g.drawLine(750, 60, 850, 60);  //颜色对应的变量
+		//g.drawLine(900, 250, 1000, 250);  //颜色对应的变量
 		j=0;
+		/*float hello = value.get(0);
+		for (int i = 1; i < value.size(); i++) { // 画出当前值
+			float geti = value.get(i);
+			// g.drawOval(j, 500-(int) geti*5, 5, 5);
+			//g.fillOval(j + 20, 500 - (int) geti * 5, 5, 5);
+			g.drawLine(j, 500 -(int)hello*5, j+5, 500-(int)geti*5);
+			j += 5;
+			hello = geti;
+		}*/
 		for(int i=0;i<value.size();i++){  //画出当前值
 			float geti = value.get(i);
-			g.drawOval(j, 500-(int) geti*5, 5, 5);
-			j+=5;
+			//g.drawOval(j, 500-(int) geti*5, 5, 5);
+			g.fillOval((int)j+40, 500-(int) geti*5, 5, 5);
+			j+=3;
 		}
-		g.setColor(Color.ORANGE);
-		g.drawLine(750, 80, 850, 80);
+		
+		g.setStroke(backup);
+		//g.setColor(Color.ORANGE);
+		g.setColor(Color.BLUE);
+		//g.drawLine(900, 300, 1000, 300);
 		j=0;
+		/*float temp = thetas.get(0);
+		for(int i=1;i<thetas.size();i++){
+			float geti = thetas.get(i);
+			//g.fillOval(j+20, 500-(int)geti*5, 5, 5);
+			g.drawLine(j+20, 500-(int)temp*5, j+25, 500-(int)geti*5);
+			j+=5;
+			temp = geti;
+		}*/
 		for(int i=0;i<thetas.size();i++){
 			float geti = thetas.get(i);
-			g.fillOval(j, 500-(int)geti*5, 5, 5);
-			j+=5;
+			g.fillOval((int)j+40, 500-(int)geti*5, 5, 5);
+			j+=3;
 		}
 	}
 	
+	public void toFile(ArrayList<Float> value, ArrayList<Float> thetas) {  ///输出到文件中
+		//将数据输出文件
+		File out = new File("C:\\Users\\Administrator\\Desktop\\trend.csv");
+		FileWriter fileWriter = null;
+		BufferedWriter bufferedWriter = null;
+		try {
+			fileWriter = new FileWriter(out);
+			bufferedWriter = new BufferedWriter(fileWriter);
+			bufferedWriter.write(new String("Time"));
+			for(int i = 1; i <= value.size(); i++){
+				if (i%10==0) {
+					bufferedWriter.write(","+Integer.toString(i/10));
+				}
+			}
+			bufferedWriter.newLine();
+			
+			bufferedWriter.write("Course");
+			for(int i = 0; i < value.size(); i++){
+				if (i%10==0) {
+					bufferedWriter.write(","+value.get(i));
+				}
+			}
+			bufferedWriter.newLine();
+			
+			bufferedWriter.write("Rudder");
+			for(int i = 0; i < thetas.size(); i++){
+				if (i%10==0) {
+					bufferedWriter.write(","+thetas.get(i));
+				}
+			}
+			bufferedWriter.newLine();
+			
+			bufferedWriter.flush();
+			bufferedWriter.close();
+			fileWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
