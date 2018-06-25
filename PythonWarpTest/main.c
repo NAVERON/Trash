289c39a80254714133ed7d8ca1340f426daefb7b
NAVERON <2478327261@qq.com>
2018-06-25 17:01:21 +0800

    test successful

--- PythonWarpTest/main.c
+++ PythonWarpTest/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "testheader.h"
+#include <dlfcn.h>
 
 int main(void)
 {
-	printf("hello python warp!");
-	double out = get_double(3, 5, 6.78);
-
+	printf("hello python warp!\n");
+	void *this = dlopen("./hello.so", RTLD_LAZY);
+	dlerror();
+	int (*fun)() = dlsym(this, "foo");
+	int (*other)() = dlsym(this, "get_double");
+	int out1 = fun(2, 3);
+	int out2 = other(4, 5, 9);
+	
+	printf("first out1 : %d \nsecond out2 : %d \n", out1, out2);
+	dlclose(this);
+	
 	return 0;
 }
 
